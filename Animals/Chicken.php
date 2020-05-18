28ed7749265cda478139fd65a64b0ba3b4d0ec67
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-05-18 15:18:52 +0700

    add all file lab

---
+++ Animals/Chicken.php
@@ -0,0 +1,15 @@
+<?php
+include_once (dirname(__FILE__) . '/../AbstractClass/Animal.php');
+include_once (dirname(__FILE__) . '/../InterfaceClass/Edible.php');
+class Chicken extends Animal
+{
+    public function makeSound()
+    {
+        return "Chicken: cluck-cluck!";
+    }
+
+    public function howToEat()
+    {
+        return "could be fried";
+    }
+}
